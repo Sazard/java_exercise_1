d87ff21fa972032e615fb32acc1d5aa13a199a74
nicolas.sutter <nicolas.sutter@epita.fr>
2021-05-17 21:54:29 +0200

    exo5

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,4 +1,10 @@
-import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.nio.file.Files;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Launcher {
     public static void main(String []args){
@@ -19,6 +25,39 @@ public class Launcher {
                     System.err.println("Ce n'est pas un entier");
                 }
                 continue;
+            }
+            if (cmd.equals("freq")){
+                System.out.println("Entrer le chemin vers votre fichier");
+                var path = scan.nextLine();
+                try {
+                    String f = Files.readString(Paths.get(path));
+                    String[] sp = f.split(" ");
+                    Map<String, Integer> ma = new HashMap<String, Integer>();
+                    for(int i = 0; i < sp.length; i++){
+                        if(ma.containsKey(sp[i])){
+                            ma.put(sp[i], ma.get(sp[i]) + 1);
+                        } else{
+                            ma.put(sp[i], 1);
+                        }
+                    }
+                    for(int zz=0; zz < 3; zz++) {
+                        String max = Collections.max(ma.entrySet(), Comparator.comparingInt(Map.Entry::getValue)).getKey();
+                        if (zz == 2){
+                            System.out.println(max);
+                            continue;
+                        }
+                        System.out.print(max + " ");
+                        ma.remove(max);
+                    }
+                } catch(IOException e){
+                    System.err.println("Unreadable file: IOException "+ e.getMessage());
+                } catch(OutOfMemoryError e1){
+                    System.err.println("Unreadable file: OutOfMemoryError " + e1.getMessage());
+                } catch(SecurityException e2){
+                    System.err.println("Unreadable file: SecurityException " + e2.getMessage());
+                }
+                continue;
+
             }
             System.out.println("Unknown command");
         }
